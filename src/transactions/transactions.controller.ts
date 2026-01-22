bbb0445d8de1beffa0cc34320d53adb183a8cb4c
Rafael Carvalho <rafacarvalho.10@outlook.com>
2026-01-22 09:25:43 -0300

    feat: implementa filtro de período por tipo e corrige lógica de recorrência

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// src/transactions/transactions.controller.ts
 import {
   Controller,
   Post,
